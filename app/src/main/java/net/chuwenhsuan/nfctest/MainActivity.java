c7df300603044b5282bcd8d934775228c8e09883
chuwenhsuan <chuwenhsuan@gmail.com>
2015-05-16 16:04:35 +0800

    App can now read the card ID.

--- app/src/main/java/net/chuwenhsuan/nfctest/MainActivity.java
+++ app/src/main/java/net/chuwenhsuan/nfctest/MainActivity.java
@@ -1,15 +1,16 @@
 package net.chuwenhsuan.nfctest;
 
+import android.content.Context;
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
-
-    public static final String TAG = "NfcDemo";
 
     private TextView mTextView;
     private NfcAdapter mNfcAdapter;
@@ -31,15 +32,37 @@ public class MainActivity extends ActionBarActivity {
 
         if (!mNfcAdapter.isEnabled()) {
             mTextView.setText("NFC is disabled.");
+            Toast.makeText(this, "Please enable NFC.", Toast.LENGTH_LONG).show();
         }
         else {
             mTextView.setText(R.string.explanation);
         }
 
-        //handleIntent(getIntent());
+        handleIntent(getIntent());
     }
 
-    /*private void handleIntent(Intent intent) {
+    private void handleIntent(Intent intent) {
+        if (getIntent().getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
+            ((TextView) findViewById(R.id.textView_explanation)).setText("ID: " + this.ByteArrayToHexString(getIntent().getByteArrayExtra(NfcAdapter.EXTRA_ID)));
+            Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(100);
+            Toast.makeText(this, "NFC ID detected: " + this.ByteArrayToHexString(getIntent().getByteArrayExtra(NfcAdapter.EXTRA_ID)) + ".", Toast.LENGTH_LONG).show();
+        }
+    }
 
-    }*/
+    private String ByteArrayToHexString(byte [] in_array) {
+        int i, buffer, input;
+        String [] hex = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+        String out= "";
+
+        for(i = 0 ; i < in_array.length ; ++i)
+        {
+            input = (int) in_array[i] & 0xff;
+            buffer = (input >> 4) & 0x0f;
+            out += hex[buffer];
+            buffer = input & 0x0f;
+            out += hex[buffer];
+        }
+        return out;
+    }
 }
