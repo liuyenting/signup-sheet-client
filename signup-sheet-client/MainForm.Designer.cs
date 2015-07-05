1faaca707e9e347aa53dab56f70c4f4f0350ccd1
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-07-05 12:59:49 +0800

    Project initialized.

---
+++ signup-sheet-client/MainForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace signup_sheet_client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cardReaderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serverStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cardReaderStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cardStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.userInfoPanel = new System.Windows.Forms.Panel();
+            this.mainStatusStrip.SuspendLayout();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainStatusStrip
+            // 
+            this.mainStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serverStatusLabel,
+            this.cardReaderStatus,
+            this.cardStatus});
+            this.mainStatusStrip.Location = new System.Drawing.Point(0, 397);
+            this.mainStatusStrip.Name = "mainStatusStrip";
+            this.mainStatusStrip.Size = new System.Drawing.Size(674, 22);
+            this.mainStatusStrip.TabIndex = 0;
+            this.mainStatusStrip.Text = "statusStrip1";
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(674, 24);
+            this.mainMenuStrip.TabIndex = 1;
+            this.mainMenuStrip.Text = "MainMenu";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cardReaderToolStripMenuItem,
+            this.serverToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.connectToolStripMenuItem.Text = "Connect";
+            // 
+            // cardReaderToolStripMenuItem
+            // 
+            this.cardReaderToolStripMenuItem.Name = "cardReaderToolStripMenuItem";
+            this.cardReaderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cardReaderToolStripMenuItem.Text = "Card Reader...";
+            // 
+            // serverToolStripMenuItem
+            // 
+            this.serverToolStripMenuItem.Name = "serverToolStripMenuItem";
+            this.serverToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.serverToolStripMenuItem.Text = "Server...";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // serverStatusLabel
+            // 
+            this.serverStatusLabel.AutoSize = false;
+            this.serverStatusLabel.Name = "serverStatusLabel";
+            this.serverStatusLabel.Size = new System.Drawing.Size(200, 17);
+            this.serverStatusLabel.Text = "Unknown";
+            this.serverStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cardReaderStatus
+            // 
+            this.cardReaderStatus.AutoSize = false;
+            this.cardReaderStatus.Name = "cardReaderStatus";
+            this.cardReaderStatus.Size = new System.Drawing.Size(150, 17);
+            this.cardReaderStatus.Text = "Unknown";
+            this.cardReaderStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cardStatus
+            // 
+            this.cardStatus.AutoSize = false;
+            this.cardStatus.Name = "cardStatus";
+            this.cardStatus.Size = new System.Drawing.Size(100, 17);
+            this.cardStatus.Text = "FFFFFF";
+            // 
+            // userInfoPanel
+            // 
+            this.userInfoPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userInfoPanel.Location = new System.Drawing.Point(0, 24);
+            this.userInfoPanel.Name = "userInfoPanel";
+            this.userInfoPanel.Size = new System.Drawing.Size(674, 373);
+            this.userInfoPanel.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 419);
+            this.Controls.Add(this.userInfoPanel);
+            this.Controls.Add(this.mainStatusStrip);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "Signup Sheet - Client";
+            this.mainStatusStrip.ResumeLayout(false);
+            this.mainStatusStrip.PerformLayout();
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip mainStatusStrip;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cardReaderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serverToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel serverStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel cardReaderStatus;
+        private System.Windows.Forms.ToolStripStatusLabel cardStatus;
+        private System.Windows.Forms.Panel userInfoPanel;
+    }
+}
+
