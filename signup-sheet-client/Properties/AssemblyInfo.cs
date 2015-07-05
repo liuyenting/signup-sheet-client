cf43b9ea5b52795dc45baa9d89d4468b054a030e
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-07-05 19:15:19 +0800

    Add more legal notice.

--- signup-sheet-client/Properties/AssemblyInfo.cs
+++ signup-sheet-client/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("signup-sheet-client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("NTU")]
 [assembly: AssemblyProduct("signup-sheet-client")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
