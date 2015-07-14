264845d9ccaa03930599a1f91dbd1cbfd65d4be9
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-07-14 19:11:38 +0800

    Create card reader wrapper.

--- signup-sheet-client/CardReader/ReaderWrapper.cs
+++ signup-sheet-client/CardReader/ReaderWrapper.cs
@@ -14,7 +14,7 @@ namespace signup_sheet_client
         class NativeMethods
         {
             [DllImport("dcrf32.dll")]
-            public static extern short dc_init(Int16 port, uint baud);
+            public static extern short dc_init(short port, uint baud);
             [DllImport("dcrf32.dll")]
             public static extern short dc_exit(int icdev);
             [DllImport("dcrf32.dll")]
@@ -27,17 +27,75 @@ namespace signup_sheet_client
         
         #endregion
 
-        private Int16 readerPort;
-        private int handler;
+        #region Card reader communication related variables.
+
+        private short readerPort = -1;
+        private int handler = -1;
 
         private const int defaultBaudRate = 115200;
 
-        public bool Open(Int16 port)
-        {
-            // Initialize the card reader.
-            this.handler =NativeMethods. dc_init(port, defaultBaudRate);
+        private const char readMode = (char)0;
 
-            return (handler > 0);
+        #endregion
+
+        private const int beepDuration = 10;
+        private short status;
+
+        public bool Open(short port)
+        {
+            // Store the port variable.
+            this.readerPort = port;
+
+            // Initialize the card reader.
+            this.handler = NativeMethods. dc_init(this.readerPort, defaultBaudRate);
+
+            return (this.handler > 0);
+        }
+        public bool Close()
+        {
+            this.status = NativeMethods.dc_exit(this.handler);
+
+            // Reset the card reader NOW, otherwise it will trigger unexpected actions.
+            this.readerPort = -1;
+            this.handler = -1;
+
+            return (this.status == 0);
+        }
+
+        public bool Alive
+        {
+            get
+            {
+                // Using version number to ping for system status.
+                // Default string length is 8 bytes.
+                return (NativeMethods.dc_getver(this.handler, new StringBuilder(64)) == 0);
+            }
+        }
+
+        public bool Beep()
+        {
+            this.status = NativeMethods.dc_beep(this.handler, beepDuration);
+            return (this.status == 0);
+        }
+
+        public bool TryRead(out string cardId)
+        {
+            // Set default value.
+            cardId = string.Empty;
+
+            ulong rawIdValue = 0;
+            this.status = NativeMethods.dc_card(this.handler, readMode, ref rawIdValue);
+
+            if(this.status == 0)
+            {
+                // Write the converted value, only if the read operation succeed.
+                cardId = Convert.ToString(rawIdValue);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
